c6d34dfaf1564dd648aaa6eb475ba2f1b2dc6dcd
Kwak04 <tronixdevelop@gmail.com>
2021-12-23 23:48:23 +0900

    feat: Added more sites

--- main.py
+++ main.py
@@ -36,10 +36,50 @@ class MainClient(discord.Client):
         # Open site
         if message_content.startswith('새나무 구글'):
             await channel.send(embed=get_site_embed(
-                url='https://www.google.com',
+                url='https://www.google.com/',
                 name='Google',
-                color=0x347deb,
+                color=0x4081ec,
                 image_url=strings['img-google-thumbnail']
+            ))
+
+        if message_content.startswith('새나무 유튜브'):
+            await channel.send(embed=get_site_embed(
+                url='https://www.youtube.com/',
+                name='YouTube',
+                color=0xf63731,
+                image_url=strings['img-youtube-thumbnail']
+            ))
+
+        if message_content.startswith('새나무 네이버'):
+            await channel.send(embed=get_site_embed(
+                url='https://www.naver.com/',
+                name='네이버',
+                color=0x03e062,
+                image_url=strings['img-naver-thumbnail']
+            ))
+
+        if message_content.startswith('새나무 온라인 클래스'):
+            await channel.send(embed=get_site_embed(
+                url='https://www.ebsoc.co.kr/',
+                name='EBS 온라인 클래스',
+                color=0xa7cd54,
+                image_url=strings['img-ebs-thumbnail']
+            ))
+
+        if message_content.startswith('새나무 클래스룸'):
+            await channel.send(embed=get_site_embed(
+                url='https://classroom.google.com/',
+                name='Google Classroom',
+                color=0x189c5c,
+                image_url=strings['img-classroom-thumbnail']
+            ))
+
+        if message_content.startswith('새나무 메가스터디'):
+            await channel.send(embed=get_site_embed(
+                url='https://www.megastudy.net/',
+                name='메가스터디',
+                color=0x007ef7,
+                image_url=strings['img-megastudy-thumbnail']
             ))
 
 
