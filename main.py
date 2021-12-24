92cdb5d9ff4a3ddcd8acdd1ab9a34f2a55d49085
Tronix Kwak <tronixdevelop@gmail.com>
2021-12-24 23:27:16 +0900

    feat: Swear words detector (#17)
    * Defined a new variable 'message_author'
    * New comment style for each feature
    * Added a UTF8 encoding option when loading json files

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import discord
 import json
 
 # Load secrets from secrets.json
-with open('secrets.json') as secrets_file:
+with open('secrets.json', encoding='UTF8') as secrets_file:
     secrets = json.load(secrets_file)
 
 # Load constant strings from strings.json
-with open('strings.json') as strings_file:
+with open('strings.json', encoding='UTF8') as strings_file:
     strings = json.load(strings_file)
 
 
@@ -41,9 +41,30 @@ class MainClient(discord.Client):
 
         # Defining to a variable
         message_content = message.content
+        message_author = message.author
         channel = message.channel
 
+        # Swear words detector
+        # This deletes the original message, and replace it with a spoiler(hidden)  message
+        #
+        swear_words = ['씨발', '싸발', 'ㅅㅂ', '시발', '좆', 'ㅈ같', '병신', 'ㅄ', 'ㅂㅅ', '쌍', '썅', 'ㅆ', '새끼']
+        for word in swear_words:
+            if word in message_content:
+                try:
+                    await message.delete()
+                except:
+                    log('ERROR', 'Unable to delete message')
+                else:
+                    embed = discord.Embed(
+                        title='욕설이 감지되었습니다.',
+                        description=strings['msg-swear-detector'].format(message),
+                        colour=0xff0000
+                    )
+                    await channel.send(embed=embed)
+                    log('욕 감지됨', f'감지된 욕: {word}', user=message_author)
+
         # Open site
+        #
         if message_content.startswith('새나무 구글'):
             site_name = 'Google'
             await channel.send(embed=get_site_embed(
@@ -52,7 +73,7 @@ class MainClient(discord.Client):
                 color=0x4081ec,
                 image_url=strings['img-google-thumbnail']
             ))
-            log('사이트 이동', site_name, user=message.author)
+            log('사이트 이동', site_name, user=message_author)
 
         if message_content.startswith('새나무 유튜브'):
             site_name = 'YouTube'
@@ -62,7 +83,7 @@ class MainClient(discord.Client):
                 color=0xf63731,
                 image_url=strings['img-youtube-thumbnail']
             ))
-            log('사이트 이동', site_name, user=message.author)
+            log('사이트 이동', site_name, user=message_author)
 
         if message_content.startswith('새나무 네이버'):
             site_name = '네이버'
@@ -72,7 +93,7 @@ class MainClient(discord.Client):
                 color=0x03e062,
                 image_url=strings['img-naver-thumbnail']
             ))
-            log('사이트 이동', site_name, user=message.author)
+            log('사이트 이동', site_name, user=message_author)
 
         if message_content.startswith('새나무 온라인 클래스'):
             site_name = 'EBS 온라인 클래스'
@@ -82,7 +103,7 @@ class MainClient(discord.Client):
                 color=0xa7cd54,
                 image_url=strings['img-ebs-thumbnail']
             ))
-            log('사이트 이동', site_name, user=message.author)
+            log('사이트 이동', site_name, user=message_author)
 
         if message_content.startswith('새나무 클래스룸'):
             site_name = 'Google Classroom'
@@ -92,7 +113,7 @@ class MainClient(discord.Client):
                 color=0x189c5c,
                 image_url=strings['img-classroom-thumbnail']
             ))
-            log('사이트 이동', 'YouTube', user=message.author)
+            log('사이트 이동', 'YouTube', user=message_author)
 
         if message_content.startswith('새나무 메가스터디'):
             site_name = '메가스터디'
@@ -102,7 +123,7 @@ class MainClient(discord.Client):
                 color=0x007ef7,
                 image_url=strings['img-megastudy-thumbnail']
             ))
-            log('사이트 이동', site_name, user=message.author)
+            log('사이트 이동', site_name, user=message_author)
 
 
 client = MainClient()
