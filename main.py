8a7cedf3293cd1f712ed561d88f3f5ea41993997
Tronix Kwak <tronixdevelop@gmail.com>
2021-12-24 22:26:48 +0900

    feat: Print bot's actions to console (#16)
    * refactor: Changed the location of the methods
    
    * refactor: Re-changed the location and added 'log' method to print information
    
    * feat<console>: Added a message when the bot logged on
    
    * refactor: Set on_message method as static method

--- main.py
+++ main.py
@@ -20,12 +20,22 @@ def get_site_embed(url, name, color, image_url=strings['img-default-site-thumbna
     return embed
 
 
+def log(title, description, user=''):
+    if user == '':
+        print(f'[{title}] {description}')
+    else:
+        print(f'{user}: [{title}] {description}')
+
+
 class MainClient(discord.Client):
     async def on_ready(self):
         activity = discord.Activity(name='새나무제', type=discord.ActivityType.playing)
         await self.change_presence(activity=activity)
 
-    async def on_message(self, message):
+        log('NOW ONLINE', f'Logged on as {self.user}')
+
+    @staticmethod
+    async def on_message(message):
         if message.author.bot or message.author == client.user:
             return  # Do nothing (Don't reply)
 
@@ -35,52 +45,64 @@ class MainClient(discord.Client):
 
         # Open site
         if message_content.startswith('새나무 구글'):
+            site_name = 'Google'
             await channel.send(embed=get_site_embed(
                 url='https://www.google.com/',
-                name='Google',
+                name=site_name,
                 color=0x4081ec,
                 image_url=strings['img-google-thumbnail']
             ))
+            log('사이트 이동', site_name, user=message.author)
 
         if message_content.startswith('새나무 유튜브'):
+            site_name = 'YouTube'
             await channel.send(embed=get_site_embed(
                 url='https://www.youtube.com/',
-                name='YouTube',
+                name=site_name,
                 color=0xf63731,
                 image_url=strings['img-youtube-thumbnail']
             ))
+            log('사이트 이동', site_name, user=message.author)
 
         if message_content.startswith('새나무 네이버'):
+            site_name = '네이버'
             await channel.send(embed=get_site_embed(
                 url='https://www.naver.com/',
-                name='네이버',
+                name=site_name,
                 color=0x03e062,
                 image_url=strings['img-naver-thumbnail']
             ))
+            log('사이트 이동', site_name, user=message.author)
 
         if message_content.startswith('새나무 온라인 클래스'):
+            site_name = 'EBS 온라인 클래스'
             await channel.send(embed=get_site_embed(
                 url='https://www.ebsoc.co.kr/',
-                name='EBS 온라인 클래스',
+                name=site_name,
                 color=0xa7cd54,
                 image_url=strings['img-ebs-thumbnail']
             ))
+            log('사이트 이동', site_name, user=message.author)
 
         if message_content.startswith('새나무 클래스룸'):
+            site_name = 'Google Classroom'
             await channel.send(embed=get_site_embed(
                 url='https://classroom.google.com/',
-                name='Google Classroom',
+                name=site_name,
                 color=0x189c5c,
                 image_url=strings['img-classroom-thumbnail']
             ))
+            log('사이트 이동', 'YouTube', user=message.author)
 
         if message_content.startswith('새나무 메가스터디'):
+            site_name = '메가스터디'
             await channel.send(embed=get_site_embed(
                 url='https://www.megastudy.net/',
-                name='메가스터디',
+                name=site_name,
                 color=0x007ef7,
                 image_url=strings['img-megastudy-thumbnail']
             ))
+            log('사이트 이동', site_name, user=message.author)
 
 
 client = MainClient()
