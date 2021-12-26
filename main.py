6693be6b2f0de6efddc92010c783338b38f46ea7
Kwak04 <tronixdevelop@gmail.com>
2021-12-27 05:41:16 +0900

    feat<console>: Save logs in log.txt

--- main.py
+++ main.py
@@ -25,9 +25,14 @@ def get_site_embed(url, name, color, image_url=strings['img-default-site-thumbna
 def log(title, description, user=''):
     current_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     if user == '':
-        print(f'{current_date} | [{title}] {description}')
+        log_text = f'{current_date} | [{title}] {description}'
+        print(log_text)
     else:
-        print(f'{current_date} | {user}: [{title}] {description}')
+        log_text = f'{current_date} | {user}: [{title}] {description}'
+        print(log_text)
+
+    with open('log.txt', 'a', encoding='UTF8') as log_file:
+        log_file.write(log_text + '\n')
 
 
 def dump_new_record(data_block, score, user):
